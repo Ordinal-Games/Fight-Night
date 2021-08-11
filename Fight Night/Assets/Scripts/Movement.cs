31bb9009af0f3e59bb95d04afc5590ecd3338cf4
Zach <luakgames@gmail.com>
2021-08-11 16:41:10 -0400

    Movement and player added
    I created a player movement script and created a temp player as well as a platform for him to move on. Player movement is basic but jumping requires a ground check, dont move or change the ground check or it would mess up the movement. Feel free to look at the code

---
+++ Fight Night/Assets/Scripts/Movement.cs
@@ -0,0 +1,73 @@
+// Code written by Ordinal Games developers //
+// Yes i know this code is bad - Zach //
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpForce;
+    private float moveInput;
+    
+    private Rigidbody2D rb;
+
+    // Jump variables //
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+
+        // Movement //
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        // Jumps //
+
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector3.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector3.up * jumpForce;
+        }
+    
+    }
+}
